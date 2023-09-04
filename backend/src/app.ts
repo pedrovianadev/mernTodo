5bd970a017b186fa39505a3ddcf6b71720bd6f23
Pedro Viana <Pedro Viana>
2023-09-04 16:01:07 -0300

    feat: add Error if the endpoint does not exists

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -16,12 +16,17 @@ app.get('/', async (_req, res, next) => {
 	
 });
 
+app.use((req, res, next) => {
+	next(Error('Endpoint not found'));
+	res.status(404);
+});
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
 	console.error(error);
 	let errorMessage = 'Something went wrong';
 	if(error instanceof Error) errorMessage = error.message;
-	return res.status(500).json({ error: errorMessage });
+	return res.status(404).json({ error: errorMessage });
 });
 
 
