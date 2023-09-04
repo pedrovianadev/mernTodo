8f616fcedb0b3ee8c9e7c28f915e27af6e9dcd55
Pedro Viana <Pedro Viana>
2023-09-04 15:45:56 -0300

    feat: noteSchema

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
+import NoteModel from './models/note';
 
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send('Hello, World');
+app.get('/', async (_req, res) => {
+	const notes = await NoteModel.find().exec();
+	return res.status(200).json(notes);
 });
+
 
 export default app;
